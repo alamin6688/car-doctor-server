63339ec441058c8bbb990f1095d13589ff100419
alamin6688 <alamin.kzs06@gmail.com>
2024-05-08 01:37:11 +0600

    booking item delete system integration added

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ app.get('/services/:id', async(req, res)=>{
       res.send(result);
     })
 
+    // Delete booking item
+    app.delete('/bookings/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
