eeeddfec1b37b8d136e2093b0f8191134b4af98b
alamin6688 <alamin.kzs06@gmail.com>
2024-05-08 00:22:41 +0600

    booking functionality added to database

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDoctor').collection('services')
-
+    const bookingCollection = client.db('carDoctor').collection('bookings')
 
 
     app.get('/services', async(req, res) =>{
@@ -43,11 +43,32 @@ app.get('/services/:id', async(req, res)=>{
     const query = { _id: new ObjectId(id)}
     const options = {
       // Include only the `title` and `imdb` fields in the returned document
-      projection: { title: 1, price: 1, service_id: 1 },
+      projection: { title: 1, price: 1, service_id: 1, img: 1 },
     };
     const result = await serviceCollection.findOne(query, options);
     res.send(result);
 })
+
+    //bookongs
+    app.get('/bookings', async(req,res)=>{
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+
+    app.post('/bookings', async(req,res)=>{
+      const booking = req.body;
+      console.log(booking)
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
